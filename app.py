2795309359ac42c69c6d9eb3e551c285912a14a6
Alterys Agency <alterys.agency@gmail.com>
2026-05-22 11:03:22 +0200

    fix port binding

--- app.py
+++ app.py
@@ -123,4 +123,5 @@ def webhook():
 
 if __name__ == '__main__':
     print("🚀 Serveur démarré sur http://localhost:5000")
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
